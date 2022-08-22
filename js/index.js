68d1425f24dbc366b136c2a28ed3a1ef7a730669
indrajith76 <indrajithgoswami@gmail.com>
2022-08-22 16:57:28 +0600

    complete calculations sections

--- js/index.js
+++ js/index.js
@@ -1,4 +1,27 @@
+function getInputFieldValueById(id){
+    let inputField = document.getElementById(id);
+    let inputFieldString = inputField.value;
+    let inputFieldValue = parseFloat(inputFieldString);
+    inputField.value = '';
+    return inputFieldValue;
+}
+
+function getElementValueById(id){
+    let element = document.getElementById(id);
+    let elementString = element.innerText;
+    let elementValue = parseFloat(elementString);
+    return elementValue;
+}
+// code for set element value
+function setElementValue(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+// code for player card selections
+let selectedPlayer = [];
 let count = 0;
+
 let playerBtn = document.querySelectorAll('.player-btn');
 for (const btn of playerBtn) {
     btn.addEventListener('click', function(event){
@@ -11,10 +34,40 @@ for (const btn of playerBtn) {
             playerList.appendChild(addPlayer);
             btn.setAttribute('disabled', true);
             btn.style.background='#5f778d';
+            selectedPlayer.push(player);
+            
         }
         else{
             alert('You already selected five players!');
         }
-
+        
     })
 }
+
+
+// code for calculate expense
+document.getElementById('calculate-btn').addEventListener('click',function(){
+    const perPlayerExpense = getInputFieldValueById('player-expense-field');
+    if(isNaN(perPlayerExpense) || perPlayerExpense <= 0) {
+        alert('Please enter a valid number');
+        return;
+    }
+    const playerExpense = selectedPlayer.length * perPlayerExpense;
+    setElementValue('player-expence-display',playerExpense);
+})
+
+document.getElementById('calculate-total').addEventListener('click', function(){
+    const allPlayerExpense = getElementValueById('player-expence-display');
+    const managerExpense = getInputFieldValueById('manager-field');
+    if(isNaN(managerExpense) || managerExpense <= 0){
+        alert('Enter a valid number in Manager input field');
+        return;
+    }
+    const coachExpense = getInputFieldValueById('coach-field');
+    if(isNaN(coachExpense) || coachExpense <= 0){
+        alert('Enter a valid number in Coach Expense input field');
+        return;
+    }
+    const calculateTotal = allPlayerExpense + managerExpense + coachExpense;
+    setElementValue('total-expense', calculateTotal);
+})
